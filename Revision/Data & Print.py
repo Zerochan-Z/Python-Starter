9344189f98119bf4a0836122f6d80e0151ec8e72
Zerochan-Z <boonfeung08@gmail.com>
2026-07-29 15:38:17 +0800

    Revision before a new life starts
    <(￣︶￣)↗[GO!]

---
+++ Revision/Data & Print.py
@@ -0,0 +1,15 @@
+name = "Avo"
+age = 19
+height = 173.0
+is_student = False
+height_meters = 173 / 100.0
+age_in_days = 19 * 365
+
+print (name)
+print (age)
+print(height)
+print (is_student)
+
+print("\nMy name is ", name , ", and my age is ", age , ", and my height is " , height , ", and it's " , is_student , " that I'm gay")
+print("My height in meters is ", height_meters)
+print("My age in days is ", age_in_days)
---
+++ Revision/If & For.py
@@ -0,0 +1,12 @@
+age = 19
+
+if age > 18: print("You're an adult")
+else : print("Kiddo")
+
+for i in range(5):
+    print (i)
+
+j = 0;
+while j <= 4:
+    print(j)
+    j = j + 1;
